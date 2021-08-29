a01eab195afb18f0e9efe7a2ff91fd5c36c3fa52
BMS Project <89306283+SWEGBatch2012-SecE-G2@users.noreply.github.com>
2021-08-29 12:45:28 +0300

    Add files via upload

--- bms.cpp
+++ bms.cpp
@@ -1,1 +1,14 @@
+// Group Members:
+// 1. Michael Gashaw
+// 2. Minase Driba
+// 3. Minase Fikadu
+// 4. Mohammed Yezid
+// 5. Nathan Lijalem
+#include <iostream>
 
+using namespace std;
+
+int main() {
+
+    return 0;
+}
