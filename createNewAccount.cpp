09f855865935c4d15f16a7bfa3e289d9ddbdfdf8
Nathaniyas <59746236+Nathaniyas@users.noreply.github.com>
2021-08-31 00:14:17 -0700

    Update createNewAccount.cpp

--- createNewAccount.cpp
+++ createNewAccount.cpp
@@ -20,8 +20,13 @@ int create_new_account_page()
  accept("\t Enter your name       |  ", &(customer[customer_counter].name), 20);
  accept("\t Enter your age        |  ", &(age), 0, 100);
  customer[customer_counter].age = age;
+ sex:
  accept("\t Enter your sex(F/M)   |  ", &(sex), 1);
- if ()
+ if (sex != 'F' || sex != 'M' || sex != 'f' || sex != 'm')
+ {
+  cout << "\t Please enter only 'F' or 'M' << endl;
+  goto sex;
+ }
  customer[customer_counter].sex = (char)sex[0];
  accept("\t Enter your phone      |  +251", &(phone), 900000000, 999999999);
  customer[customer_counter].phone = phone;
@@ -49,10 +54,28 @@ int create_new_account_page()
  }
  cout << setfill('-') << setw(14 * 4 + 4) << "-" << endl;
  cout << setfill(' ');
-
+accType:
  accept("\t Account type(1-4)     |  ", &acc_type, 1, 4);
+ if (age > 18 && acc_type == 3)
+ {
+  cout << "You have to be under 18 to use this type." << endl
+   << "Please select another account type!";
+  goto accType;
+ }
+ if (age > 5 && acc_type == 2)
+ {
+  cout << "You have to be under 5 to use this type." << endl
+   << "Please select another account type!";
+  goto accType;
+ }
  customer[customer_counter].acc_type = acc_type;
- accept("\t Set a strong password |  ", &(customer[customer_counter].password), 8);
+ password:
+ accept("\t Set a strong password |  ", &(customer[customer_counter].password), 16);
+ if (password.length() < 6 || password.length() > 8)
+ {
+  cout << "Set you password between 6 and 8 characters." << endl;
+  goto password;
+ }
  customer[customer_counter].acc_no = initial_acc_no++;
  accept("\t Enter initial amount  |  ", &balance, acc_types[customer[customer_counter].acc_type - 1].initial, 1000);
  customer[customer_counter].balance = balance;
