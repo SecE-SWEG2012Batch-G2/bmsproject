4a06f95e2210b9d062461a4d3c13c34fd0e5cafe
Nathaniyas <59746236+Nathaniyas@users.noreply.github.com>
2021-08-31 21:26:38 +0300

    Update deactivate_page_function.cpp

--- deactivate_page_function.cpp
+++ deactivate_page_function.cpp
@@ -3,7 +3,7 @@ int deactivate_page()
 	int decision;
 	int newIndex = 0;
 	cout << endl;
-	accept("\t Do you really want to deactivate your account?\n\t     1. YES\n\t     2. NO\n\tEnter option number(1-2)", &decision, 1, 2);
+	accept("\tDo you really want to deactivate your account?\n\t     1. YES\n\t     2. NO\n\tEnter option number(1-2): ", &decision, 1, 2);
 	if (decision == 1)
 	{
 		for (int i = 0; i < customer_counter; i++)
@@ -15,7 +15,8 @@ int deactivate_page()
 			}
 		}
 		customer[customer_counter - 1] = {};
-		cout << "You successfully deactivated your account. << endl;
+		customer_counter--;
+		cout << "You successfully deactivated your account." << endl;
 		cout << "Enter any key to proceed to main menu: ";
 		cin >> option;
 		return 1;
